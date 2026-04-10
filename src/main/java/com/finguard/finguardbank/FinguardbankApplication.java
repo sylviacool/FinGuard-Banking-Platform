8d2efe88b654cb7a15c1ba6b70bf92c1be71d91e
Ogechi Sylvia Uzoma <sylviacool95@gmail.com>
2026-04-10 17:54:38 +0200

    Connected to Database

---
+++ src/main/java/com/finguard/finguardbank/FinguardbankApplication.java
@@ -0,0 +1,13 @@
+package com.finguard.finguardbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinguardbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinguardbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finguard/finguardbank/FinguardbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finguard.finguardbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinguardbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
